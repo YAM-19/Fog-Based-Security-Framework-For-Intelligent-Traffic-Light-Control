2b1f27fdaa0cab82fff5b87f3a4751e1f201022b
YAMINI ARIKATLA <yaminiarikatla@gmail.com>
2026-08-05 16:55:09 +0530

    Delete ids.py

--- ids.py
+++
@@ -1,6 +0,0 @@
-def detect_intrusion(vehicle_count, speed):
-    if vehicle_count > 150:
-        return "Possible DoS Attack"
-    if speed < 5:
-        return "Sensor Spoofing Detected"
-    return "No Intrusion"
